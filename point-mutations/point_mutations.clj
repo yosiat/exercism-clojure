83934851dc4a4328d1026b8d608bb9fad3c23067
Yosi Attias <yosy101@gmail.com>
2014-11-30 21:17:53 +0200

    Refactor point mutations - removing zip

--- point-mutations/point_mutations.clj
+++ point-mutations/point_mutations.clj
@@ -4,10 +4,6 @@
   "Sum a list of numbers"
   (reduce + lst))
 
-(defn zip [first-lst second-lst]
-  "Returns a vector where those to letters are zipped"
-  (map vector first-lst second-lst))
-
 (defn letters-distance [first-letter second-letter]
   "Returns the distance between two letters, if
   they are equals the distance will 0 otherwise it will be 1"
@@ -17,7 +13,7 @@
   "Returns the hamming distance between two strings, if there length are
   equall nill will be returned"
   (if (= (count first-str) (count second-str))
-    (sum (map #(letters-distance (first %) (second %)) (zip first-str second-str)))
+    (sum (map #(letters-distance %1 %2) first-str second-str))
     nil))
 
 
